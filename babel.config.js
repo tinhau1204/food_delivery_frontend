ea3cbbeb9ebb3c609dfe7302810f5a98480d01a2
foxthehuman <foxthehuman1900@gmail.com>
2023-05-29 13:30:43 +0700

    fix config files

--- babel.config.js
+++ babel.config.js
@@ -1,48 +1,10 @@
-// module.exports = {
-// presets: [
-//   [
-//     "@babel/preset-react",
-//     {
-//       runtime: "automatic",
-//     },
-//   ],
-// ],
-// };
-module.exports = function (api) {
-  api.cache(true);
-  api.assertVersion("^7.4.5");
-
-  const presets = [
+module.exports = {
+  presets: [
     [
       "@babel/preset-react",
       {
         runtime: "automatic",
       },
     ],
-  ];
-  const plugins = [
-    [
-      "babel-plugin-import",
-      {
-        libraryName: "@mui/material",
-        libraryDirectory: "",
-        camel2DashComponentName: false,
-      },
-      "core",
-    ],
-    [
-      "babel-plugin-import",
-      {
-        libraryName: "@mui/icons-material",
-        libraryDirectory: "",
-        camel2DashComponentName: false,
-      },
-      "icons",
-    ],
-  ];
-
-  return {
-    presets,
-    plugins,
-  };
+  ],
 };
